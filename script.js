82e1ffefe4689ab1d293df19d3acd969788929f2
milkpotcat-sudo <milk.pot.cat@gmail.com>
2026-05-22 12:35:33 +0900

    Update script.js

--- script.js
+++ script.js
@@ -507,6 +507,7 @@ document
 };
 
 
+
 let newConfirm = false;
 
 document
@@ -631,3 +632,46 @@ border-radius:12px;
 });
 
 };
+
+function updateDate(){
+
+const now =
+new Date();
+
+let year =
+now.getFullYear();
+
+let month =
+String(
+now.getMonth()+1
+).padStart(2,"0");
+
+let day =
+String(
+now.getDate()
+).padStart(2,"0");
+
+let hour =
+now.getHours();
+
+const minute =
+String(
+now.getMinutes()
+).padStart(2,"0");
+
+if(hour < 6){
+
+hour += 24;
+
+}
+
+document
+.getElementById(
+"dateText"
+).innerText =
+`${year}/${month}/${day} ${hour}:${minute}`;
+
+}
+
+updateDate();
+
